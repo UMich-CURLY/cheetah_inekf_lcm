7e92a175ba764d2ac188dcc1d253d1fc3ae2a3e8
Tingjun-Li <tingjunl@umich.edu>
2021-12-09 23:57:09 -0500

    Added two documentation files. Untrack build folder

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -43,6 +43,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/usr/share/cmake-3.22/Modules/CMakeTestCCompiler.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeTestCXXCompiler.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeTestCompilerCommon.cmake"
+  "/usr/share/cmake-3.22/Modules/CMakeUnixFindMake.cmake"
   "/usr/share/cmake-3.22/Modules/Compiler/ADSP-DetermineCompiler.cmake"
   "/usr/share/cmake-3.22/Modules/Compiler/ARMCC-DetermineCompiler.cmake"
   "/usr/share/cmake-3.22/Modules/Compiler/ARMClang-DetermineCompiler.cmake"
